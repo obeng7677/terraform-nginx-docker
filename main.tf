123346dd721175ac8cac87ae3123d7ec3609abff
Obeng <obeng7677@gmail.com>
2025-08-02 23:23:07 +0100

    Initial commit: Terraform + Docker + Nginx

---
+++ main.tf
@@ -0,0 +1,25 @@
+
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.2"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx_server" {
+  name  = "nginx-server"
+  image = docker_image.nginx.name
+
+  ports {
+    internal = 80
+    external = 8081
+  }
+}
